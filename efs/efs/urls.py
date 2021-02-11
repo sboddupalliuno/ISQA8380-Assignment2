49f709527925745b197741f7bceafb31e0142d93
Shweta Boddupalli <sboddupalli@unomaha.edu>
2021-02-10 22:47:30 -0600

    Update urls.py

--- efs/efs/urls.py
+++ efs/efs/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
     path('', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
